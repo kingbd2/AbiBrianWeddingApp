bb22a53a0167c2ea7d914dedccb52397def67a52
Brian King <kingbd2@gmail.com>
2019-05-13 22:18:25 -0400

    Added navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,47 @@
 <template>
-  <section class="container">
-    <div>
-      <!-- <logo /> -->
-      <!-- <div class="columns is-centered is-vcentered">
-        <div class="column has-text-centered is-one-third">
-          <div class="textcontainer">
-            <h1 class="title has-text-primary">Shmells and Marty</h1>
-            <h2 class="subtitle has-text-info">The queen and king of cheesy</h2>
-          </div>
+  <div>
+    <navbar></navbar>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Primary title
+          </h1>
+          <h2 class="subtitle">
+            Primary subtitle
+          </h2>
         </div>
-        <div class="column is-one-third is-offset-2 has-text-centered">
-          <figure class="image is-256x256">
-            <img class="is-rounded" src="../../assets/images/brianandabi/brianandabi4.jpg" alt="Shmells and Marty">
-          </figure>
-        </div>
-      </div> -->
-      <!-- <figure class="image is-128x128">
-        <img src="../assets/images/front.png">
-      </figure> -->
-      <figure>
-        <img
-          src="https://berqdg.bn.files.1drv.com/y4ml13TGhWP7VNsb9cqUVcto_Hhmb7uVQPhLI5fIwKd1Qf2NvqOZBdu6W65bMcTil5UGo3u9gVcT2XW6FO46skmjMn1yNxcgdQ7UjlHAUvn_PxUJZqd3m-qE97IhsPyYVup3n64PmvaRCmrMCqsflCUu7Mahd4nEdiD2fPbn1U0bifT_WHc8dGPRJBzaBMVbwDrPwYjwBRduvMAgOV0sjQE-w?width=2428&height=1366&cropmode=none"
-          width="400" height="427" />
-      </figure>
+      </div>
+    </section>
 
-      <h1 class="title">
-        We're getting married!
-      </h1>
-      <h2 class="subtitle">
-        And we want to celebrate with you.
-      </h2>
-      <!-- <section class="hero is-primary">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">
-              Primary title
-            </h1>
-            <h2 class="subtitle">
-              Primary subtitle
-            </h2>
-          </div>
-        </div>
-      </section> -->
-    </div>
-  </section>
+    <section class="container">
+      <div>
+        <figure>
+          <img
+            src="https://berqdg.bn.files.1drv.com/y4ml13TGhWP7VNsb9cqUVcto_Hhmb7uVQPhLI5fIwKd1Qf2NvqOZBdu6W65bMcTil5UGo3u9gVcT2XW6FO46skmjMn1yNxcgdQ7UjlHAUvn_PxUJZqd3m-qE97IhsPyYVup3n64PmvaRCmrMCqsflCUu7Mahd4nEdiD2fPbn1U0bifT_WHc8dGPRJBzaBMVbwDrPwYjwBRduvMAgOV0sjQE-w?width=2428&height=1366&cropmode=none"
+            width="400" height="427" />
+        </figure>
+
+        <h1 class="title">
+          We're getting married!
+        </h1>
+        <h2 class="subtitle">
+          And we want to celebrate with you.
+        </h2>
+
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
   import Logo from '~/components/Logo.vue'
+  import Navbar from '~/components/Navbar.vue'
 
   export default {
     components: {
-      Logo
+      Logo,
+      Navbar
     },
     mounted() {
       console.log(this.$route.params.slug);
