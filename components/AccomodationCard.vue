330fe7bf52b7162bd69035ff00d9d01965a04fe6
brian King <kingbd2@googlemail.com>
2019-06-13 20:45:07 -0400

    Final touches - accomodations formatting

--- components/AccomodationCard.vue
+++ components/AccomodationCard.vue
@@ -10,13 +10,13 @@
                 <div class="media">
                     <div class="media-content">
                         <p class="title is-4 no-padding has-text-primary">{{ details.title }}</p>
-                        </br>
-                        <p class="subtitle is-6 has-text-info has-text-weight-bold">{{ details.dates }}</p>
-                        <p class="subtitle is-6 has-text-info has-text-weight-bold">{{ details.price }}</p>
-                        <p class="subtitle is-6 has-text-info">{{ details.bookby }}</p>
-                        <p class="subtitle is-6 has-text-info">{{ details.location }}</p>
-                        <p class="subtitle is-6 has-text-info">{{ details.transportation }}</p>
-                        <p class="subtitle is-6 has-text-info">{{ details.bookhere }}</p>
+                        <!-- </br> -->
+                        <p class="subtitle is-6 has-text-info has-text-weight-bold" v-if="details.dates">{{ details.dates }}</p>
+                        <p class="subtitle is-6 has-text-info has-text-weight-bold" v-if="details.price">{{ details.price }}</p>
+                        <p class="subtitle is-6 has-text-info" v-if="details.bookby">{{ details.bookby }}</p>
+                        <p class="subtitle is-6 has-text-info" v-if="details.location">{{ details.location }}</p>
+                        <p class="subtitle is-6 has-text-info" v-if="details.transportation">{{ details.transportation }}</p>
+                        <p class="subtitle is-6 has-text-info" v-if="details.bookhere">{{ details.bookhere }}</p>
                         <div v-for="item in details.info" :key="item.id">
                             <div v-if="item.isLink === false">
                                 <div class="is-6 has-text-info"> {{ item.name }}</div>
@@ -48,6 +48,6 @@
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
