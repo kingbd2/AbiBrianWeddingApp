625721ecda7596774b510a559afb08dd72a38d7b
brian King <kingbd2@googlemail.com>
2019-07-01 16:17:25 -0400

    Commit before putting into production

--- store/api/session.js
+++ store/api/session.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 // const host_with_port = host.concat(':8000');
 const dev_host_with_port = '//localhost:8000';
 const prod_host_with_port = '//abiellaandbriangetmarried.ca';
-const base_path = dev_host_with_port.concat('/api/v1/');
-// const base_path = prod_host_with_port.concat('/api/v1/');
+// const base_path = dev_host_with_port.concat('/api/v1/');
+const base_path = prod_host_with_port.concat('/api/v1/');
 
 const CSRF_COOKIE_NAME = 'csrftoken';
 const CSRF_HEADER_NAME = 'X-CSRFToken';
